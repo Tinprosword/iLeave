ebdd49639162768a3bee4bb2fd9536e1bff67597
linson <lsfv@163.com>
2022-04-08 10:45:29 +0800

    temp save

--- WEBUI/Pages/approval_wait.aspx.cs
+++ WEBUI/Pages/approval_wait.aspx.cs
@@ -629,12 +629,12 @@ namespace WEBUI.Pages
             if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
             {
                 //set tr_left tr_right\
-                var trleft = e.Item.FindControl("tr_left");
-                var trright = e.Item.FindControl("tr_right");
-                if (trleft == null || trright == null)
-                {
-                    return;
-                }
+                //var trleft = e.Item.FindControl("tr_left");
+                //var trright = e.Item.FindControl("tr_right");
+                //if (trleft == null || trright == null)
+                //{
+                //    return;
+                //}
 
                 if (e.Item.DataItem is WebServiceLayer.WebReference_leave.LeaveRequestMaster == false)
                 {
@@ -649,15 +649,63 @@ namespace WEBUI.Pages
                     return;
                 }
 
-                if (attachments.Count >= 8)
+                //if (attachments.Count >= 8)
+                //{
+                //    trleft.Visible = true;
+                //    trright.Visible = true;
+                //}
+                //else
+                //{
+                //    trleft.Visible = false;
+                //    trright.Visible = false;
+                //}
+
+
+                //set attach list
+                var ltattachment = e.Item.FindControl("lt_attachlist");
+                if (ltattachment == null || ltattachment is Literal == false)
                 {
-                    trleft.Visible = true;
-                    trright.Visible = true;
+                    return;
                 }
-                else
+
+
+                var ltattachment_lt = (Literal)ltattachment;
+                ltattachment_lt.Text = BLL.common.GetAttachmentHtml(attachments);
+
+                ////set scroll function for each item.
+                //var btnleft = e.Item.FindControl("btnleft");
+                //var btnright = e.Item.FindControl("tr_right");
+                //var divcontent = e.Item.FindControl("fullf");
+
+                //if (btnleft == null || btnright == null || divcontent == null || trleft.Visible == false || trright.Visible==false)
+                //{}
+                //else
+                //{
+                //    string scrollJs = "SetSroll(\"#{0}\", \"#{1}\", \"#{2}\", {3});";
+                //    scrollJs = string.Format(scrollJs, btnleft.ClientID, btnright.ClientID, divcontent.ClientID, 40);
+                //    lt_jsScroll.Text += scrollJs;
+                //}
+            }
+
+        }
+
+
+        protected void rp_clot_list_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            //1get item index. 2 get ref data. 3.get attach data. 4.do something.
+            if (e.Item.ItemType == ListItemType.Item || e.Item.ItemType == ListItemType.AlternatingItem)
+            {
+                if (e.Item.DataItem is WebServiceLayer.WebReference_leave.StaffCLOTRequest == false)
                 {
-                    trleft.Visible = false;
-                    trright.Visible = false;
+                    return;
+                }
+
+                WebServiceLayer.WebReference_leave.StaffCLOTRequest thedata = (WebServiceLayer.WebReference_leave.StaffCLOTRequest)e.Item.DataItem;
+
+                List<MODEL.App_AttachmentInfo> attachments = BLL.Leave.getAttendanceModel(loginer.loginName, thedata.ID, Server, GlobalVariate.AttachType.clot);
+                if (attachments == null)
+                {
+                    return;
                 }
 
 
@@ -672,21 +720,9 @@ namespace WEBUI.Pages
                 var ltattachment_lt = (Literal)ltattachment;
                 ltattachment_lt.Text = BLL.common.GetAttachmentHtml(attachments);
 
-                //set scroll function for each item.
-                var btnleft = e.Item.FindControl("btnleft");
-                var btnright = e.Item.FindControl("tr_right");
-                var divcontent = e.Item.FindControl("fullf");
 
-                if (btnleft == null || btnright == null || divcontent == null || trleft.Visible == false || trright.Visible==false)
-                {}
-                else
-                {
-                    string scrollJs = "SetSroll(\"#{0}\", \"#{1}\", \"#{2}\", {3});";
-                    scrollJs = string.Format(scrollJs, btnleft.ClientID, btnright.ClientID, divcontent.ClientID, 40);
-                    lt_jsScroll.Text += scrollJs;
-                }
             }
-
         }
+
     }
 }
