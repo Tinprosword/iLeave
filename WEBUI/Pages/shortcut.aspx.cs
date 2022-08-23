4b246e2ec71853426e1bc5208c8dddd37f85e94b
linson <lsfv@163.com>
2022-08-23 18:01:05 +0800

    pause.

---
+++ WEBUI/Pages/shortcut.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WEBUI.Pages
+{
+    public partial class shortcut:System.Web.UI.Page
+    {
+        private string mQSurl_name = "url";
+        private string mQSurl_value = "";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(Request.QueryString[mQSurl_name]))
+            {
+                mQSurl_value =  Request.QueryString[mQSurl_name];
+                mQSurl_value = HttpUtility.HtmlEncode(mQSurl_value);
+            }
+
+            LSLibrary.WebAPP.LoginUser<MODEL.UserInfo> loginer = LSLibrary.WebAPP.LoginManager.GetLoinger<MODEL.UserInfo>();
+            if (loginer != null && loginer.userInfo != null)
+            {
+                Response.Redirect(mQSurl_value, true);
+            }
+            else
+            {
+                
+            }
+        }
+
+
+        
+    }
+}
