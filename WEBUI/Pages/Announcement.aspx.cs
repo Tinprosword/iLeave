445f13e7ee38201ebcc94cdbe91d9c954a3bf6f5
linson <lsfv@163.com>
2023-08-08 11:06:14 +0800

    page is ok. next: 1. show message. 2.download. 3.android push message.

--- WEBUI/Pages/Announcement.aspx.cs
+++ WEBUI/Pages/Announcement.aspx.cs
@@ -7,11 +7,30 @@ using System.Web.UI.WebControls;
 
 namespace WEBUI.Pages
 {
-    public partial class Announcement : System.Web.UI.Page
+    public partial class Announcement : BLL.CustomLoginTemplate
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+        #region page Event
+        protected override void InitPage_OnBeforeF5RegisterEvent()
+        { }
 
+        protected override void InitPage_OnEachLoadAfterCheckSessionAndF5_1()
+        {
         }
+
+        protected override void InitPage_OnFirstLoad2()
+        { }
+
+        protected override void PageLoad_Reset_ReInitUIOnEachLoad3()
+        { }
+
+        protected override void PageLoad_InitUIOnFirstLoad4()
+        {
+            ((WEBUI.Controls.leave)this.Master).SetupNaviagtion(true, BLL.MultiLanguageHelper.GetLanguagePacket().CommonBack, BLL.MultiLanguageHelper.GetLanguagePacket().apply_menu_current, "~/pages/main.aspx", true);
+        }
+
+        protected override void PageLoad_Reset_ReInitUIOnEachLoad5()
+        { }
+
+        #endregion
     }
 }
